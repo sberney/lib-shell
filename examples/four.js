64f9b4adf9ae2ee665f351cc9994532d2c00167d
sberney <sam@samuelberney.com>
2018-08-04 00:04:27 -0700

    defunctify

--- examples/four.js
+++ examples/four.js
@@ -1,7 +1,10 @@
-//const { exec } = require('../index');
-//exec('echo "hello world!"', {
-//  plain: true,
-//  info: false
-//});
-//
-//// "hello world!"
+// DEFUNCT: no trailing newline doesn't work
+
+const { exec } = require('../index');
+exec('echo "hello world!"', {
+  plain: true,
+  info: false,
+  noTrailingNewline: true
+});
+
+// "hello world!"

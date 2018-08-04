148d06de8efc9195b5920fb9bf5f6b687104e906
sberney <sam@samuelberney.com>
2018-08-04 03:15:31 -0700

    No jspm

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,10 @@ const gulp = require('gulp');
 const concat = require('gulp-concat');
 
 const Builder = require('systemjs-builder');
-const builder = new Builder('./', 'config.js');
 
-gulp.task('compile', () => 
-  builder.buildStatic('index.js', 'build/lib-shell.js', {
+gulp.task('compile', () => {
+  const builder = new Builder('./', 'config.js');
+  return builder.buildStatic('index.js', 'build/lib-shell.js', {
     format: 'cjs',
     minify: false,
     externals: [
@@ -14,7 +14,8 @@ gulp.task('compile', () =>
       'process',
       'os'
     ]
-  }));
+  })
+});
 
 gulp.task('build', ['compile'], () => {
   return gulp.src([
@@ -22,5 +23,29 @@ gulp.task('build', ['compile'], () => {
     './build/lib-shell.js'
   ]).
   pipe(concat('lib-shell.js')).
+  pipe(gulp.dest('dist/'));
+});
+
+gulp.task('transpiled-compile', () => {
+  const builder = new Builder('./', 'jspm.config.js');
+  builder.loadConfig('./config.js');
+  return builder.buildStatic('index.js', 'build/lib-shell.es5.js', {
+    format: 'cjs',
+    minify: false,
+    externals: [
+      'path',
+      'child_process',
+      'process',
+      'os'
+    ]
+  })
+});
+
+gulp.task('transpile', ['transpiled-compile'], () => {
+  return gulp.src([
+    './LICENSE',
+    './build/lib-shell.es5.js'
+  ]).
+  pipe(concat('lib-shell.es5.js')).
   pipe(gulp.dest('dist/'));
 });

0c09b506ad57a47a303cacc67c93f2c1608c5103
sberney <sam@samuelberney.com>
2018-10-11 22:34:28 -0700

    test for eight.js

---
+++ test/exit-true.js
@@ -0,0 +1,25 @@
+const { expect } = require('chai');
+const { EOL } = require('os');
+const { exec } = require('../index');
+const concat = require('async-concat-stream');
+
+// eight.js
+
+describe('exit: true', () => {
+
+  // todo: can't run because it exits the node tests :)
+  // ... but we can directly spawn a process and check its exit code.
+
+  xit('totally exits the node process', async () => {
+
+    const result = await exec('exit 9', {
+      exit: true,
+      stdio: { stdout: concat() }
+    }).
+    catch(err => Promise.resolve(err));
+
+    expect(result).to.be.an.instanceOf(Error);
+    expect(typeof result.code).to.equal('number');
+    expect(result.code).to.equal(9);
+  });
+});

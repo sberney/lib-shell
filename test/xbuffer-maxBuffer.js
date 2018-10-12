0358fe80219a996e577e4a832cf8945deefe6b03
sberney <sam@samuelberney.com>
2018-10-11 23:12:19 -0700

    test twelve.js

---
+++ test/xbuffer-maxBuffer.js
@@ -0,0 +1,23 @@
+const { expect } = require('chai');
+const { EOL } = require('os');
+const { exec } = require('../index');
+const concat = require('async-concat-stream');
+
+// twelve.js
+
+describe('xbuffer with maxBuffer', () => {
+  it("doesn't allow both options simultaneously", async () => {
+    const stream = concat();
+
+    const result = await exec('echo "hello world!"', {
+      xbuffer: 0.01,
+      maxBuffer: 10,
+      stdio: { stdout: stream, stderr: stream }
+    }).
+    catch(err => err);
+    stream.end();
+
+    expect(await result).to.be.an.instanceof(Error);
+    expect(await result).to.match(/Cannot set both/);
+  });
+});

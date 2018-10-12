bd6528ef7e667f6df2bf54c2f85203a330fc8c5a
sberney <sam@samuelberney.com>
2018-10-11 23:06:39 -0700

    test eleven.js -- prefixedBanner

---
+++ test/prefixed-failure-banner.js
@@ -0,0 +1,37 @@
+const { expect } = require('chai');
+const { EOL } = require('os');
+const { exec } = require('../index');
+const concat = require('async-concat-stream');
+
+// eleven.js
+
+describe('prefixedBanner: true', () => {
+  it('prefixes the failure banner', async () => {
+    const stream = concat();
+
+    await exec('exit 1', {
+      stdio: { stdout: stream, stderr: stream },
+      prefixedBanner: true,
+      failureBanner: `
+Build failed.
+Try checking out a fresh copy and building it from scratch.
+If that doesn't work, email tech@ITsupport.utopiacorp.net. `
+    }).
+    catch(err => Promise.resolve(err));
+    stream.end();
+
+    expect(await stream.promise).to.equal(
+      [
+        '',
+        '[shell] shell exited, code 1.',
+        '[shell] ',
+        '[shell] ', // todo: unexpected newline
+        '[shell] Build failed.',
+        '[shell] Try checking out a fresh copy and building it from scratch.',
+        "[shell] If that doesn't work, email tech@ITsupport.utopiacorp.net. ",
+        '[shell] ',
+        ''
+      ].join(EOL)
+    );
+  });
+});

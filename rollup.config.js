d2a824e233297ff07a79d61689f7d090fe33f4ae
sberney <sam@samuelberney.com>
2018-10-12 23:42:03 -0700

    Attempt to rollup -- whoops, doh, cjs imports

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,6 @@
 import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default [
   {
@@ -16,6 +18,12 @@ export default [
       format: 'es',
       sourcemap: true
     },
-    plugins: [ babel() ]
+    plugins: [
+      resolve(),
+      commonjs(),
+      babel({
+        exclude: 'node_modules/**'
+      })
+    ]
   }
 ]

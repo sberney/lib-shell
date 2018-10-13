b4be65cf932e8577d46a05b6f1c0c6e326bebe44
sberney <sam@samuelberney.com>
2018-10-13 02:02:01 -0700

    Working on gulp -- lodash-es is very prickly

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,46 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
-const { exec } = require('./index');
+const merge = require('merge-stream');
+const { exec } = require('./polyfill-index');
 
-const Builder = require('systemjs-builder');
+const licenseArray = [
+  './src/COMMENT/BEGIN',
+  './LICENSE',
+  './src/COMMENT/LODASH',
+  './node_modules/lodash/LICENSE',
+  './src/COMMENT/END',
+];
 
-gulp.task('compile', () => {
-  const builder = new Builder('./', 'config.js');
-  return builder.buildStatic('index.js', 'build/lib-shell.js', {
-    format: 'cjs',
-    minify: false,
-    externals: [
-      'path',
-      'child_process',
-      'process',
-      'os'
-    ]
-  })
-});
+gulp.task('compile', () =>
+   exec('yarn rollup'));
 
-gulp.task('build-es6', ['compile'], () => {
-  return gulp.src([
-    './src/COMMENT/BEGIN',
-    './LICENSE',
-    './src/COMMENT/LODASH',
-    './node_modules/lodash/LICENSE',
-    './src/COMMENT/SYSTEMJS',
-    './node_modules/systemjs/LICENSE',
-    './src/COMMENT/END',
-    './build/lib-shell.js'
+gulp.task('distribute', ['compile'], () => {
+
+  return merge([
+    gulp.src(licenseArray.concat([
+      './build/lib-shell.js'
+    ])).
+    pipe(concat('lib-shell.js')),
+
+    gulp.src(licenseArray.concat([
+      './build/lib-shell.min.js'
+    ])).
+    pipe(concat('lib-shell.min.js')),
+
+    gulp.src(licenseArray.concat([
+      './build/lib-shell.cjs.js'
+    ])).
+    pipe(concat('lib-shell.cjs.js')),
+
+    gulp.src(licenseArray.concat([
+      './build/lib-shell.es5.js'
+    ])).
+    pipe(concat('lib-shell.es5.js')),
   ]).
-  pipe(concat('lib-shell.js')).
   pipe(gulp.dest('dist/'));
 });
 
-gulp.task('transpiled-compile', ['compile'], () =>
-   exec('yarn babel'));
-
-gulp.task('build-es5', ['transpiled-compile'], () => {
-  return gulp.src([
-    './src/COMMENT/BEGIN',
-    './LICENSE',
-    './src/COMMENT/LODASH',
-    './node_modules/lodash/LICENSE',
-    './src/COMMENT/SYSTEMJS',
-    './node_modules/systemjs/LICENSE',
-    './src/COMMENT/END',
-    './build/lib-shell.es5.js'
-  ]).
-  pipe(concat('lib-shell.es5.js')).
-  pipe(gulp.dest('dist/'));
-});
 
 gulp.task('build', [
-  'build-es6',
-  'build-es5'
+  'distribute',
 ]);

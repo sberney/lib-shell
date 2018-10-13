1198a0687ac2fffc9649901a805f992a6e6be99f
sberney <sam@samuelberney.com>
2018-10-13 01:44:25 -0700

    build-sanity-abstractions

---
+++ test/build-sanity-es5.js
@@ -0,0 +1,6 @@
+const { createBuildSanityTest } = require('./lib/build-sanity');
+
+createBuildSanityTest({
+  buildName: 'es5 build',
+  requirePath: '../dist/lib-shell.es5.js'
+});
---
+++ test/lib/build-sanity.js
@@ -0,0 +1,43 @@
+const { expect } = require('chai');
+const path = require('path');
+const { EOL } = require('os');
+const concat = require('async-concat-stream');
+
+const createBuildSanityTest = ({ buildName, requirePath }) => {
+
+  describe(buildName, () => {
+
+    let LibShell;
+    before(() => {
+      LibShell = require(path.resolve('..', requirePath));
+    });
+
+    it('exports function exec', () => {
+      expect(typeof LibShell.exec).to.equal('function');
+    });
+
+    it('exports function createExec', () => {
+      expect(typeof LibShell.createExec).to.equal('function');
+    });
+
+    it('exec performs basic functionality', async () => {
+      const stream = concat();
+
+      await LibShell.exec('echo "hello world!"', {
+        stdio: { stdout: stream }
+      });
+      stream.end();
+
+      expect(await stream.promise).to.equal(
+        [
+          '[shell] "hello world!"',
+          '[shell] ',
+          '[shell] shell exited, code 0.',
+          ''
+        ].join(EOL)
+      );
+    });
+  });
+};
+
+module.exports = { createBuildSanityTest };
